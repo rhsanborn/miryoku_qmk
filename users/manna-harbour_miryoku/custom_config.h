8eca0b5663673c4a4a0028d332862d71d3d378a9
Randall Sanborn <rhsanborn@gmail.com>
2025-09-12 12:53:23 -0400

    Fix custom config

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -26,7 +26,7 @@ MIRYOKU_X(GAMENUM,"GameNum") \
 KC_Q,             KC_W,             KC_F,             KC_P,             KC_B,             KC_J,             KC_L,             KC_U,             KC_Y,             KC_SQT,           \
 KC_A,             KC_R,             KC_S,             KC_T,             KC_G,             KC_M,             KC_N,             KC_E,             KC_I,             KC_O,             \
 KC_Z,             KC_X,             KC_C,             KC_D,             KC_V,             KC_K,             KC_H,             KC_COMM,         KC_DOT,           KC_SLSH,         \
-U_NP,              U_NP,              KC_ESC,           KC_SPC,      U_LT(U_GAMENUM, KC_TAB), KC_RET,       HYPR_T(BSPC),     TD(U_TD_U_BASE),   U_NP,              U_NP
+U_NP,              U_NP,              KC_ESC,           KC_SPC,     LT(U_GAMENUM, KC_TAB), KC_RET,       HYPR_T(BSPC),     TD(U_TD_U_BASE),   U_NP,              U_NP
 
 #define MIRYOKU_LAYER_GAMENUM \
 KC_F1,            KC_F2,            KC_F3,            KC_F4,            KC_TILD,         KC_LBRC,          KC_F7,            KC_F8,            KC_F9,            KC_RBRC,         \
